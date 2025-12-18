52d93cf6b00e9c8623a70698b1fe6bd976741aba
Daln.dev <sdp@daln.dev>
2025-12-17 22:07:52 -0500

    feat: update dependencies and implement main layout
    - Added new dependencies: @angular/cdk, @floating-ui/dom, @supabase/supabase-js, @tanstack/angular-table, @tanstack/angular-virtual, ng-primitives.
    - Removed placeholder content from app.html and replaced it with <main-layout/> component.
    - Created MainLayout component with basic structure.
    - Introduced Supabase service for future integration.
    - Added Complaint module components and service for handling complaints.
    - Created empty components for Defendant module.
    - Updated app.ts to include MainLayout in imports.

---
+++ src/app/layouts/main-layout/main-layout.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'main-layout',
+  templateUrl: './main-layout.html',
+  styleUrls:  ['./main-layout.css'],
+  imports: [
+    CommonModule
+  ]
+})
+export class MainLayout { }
